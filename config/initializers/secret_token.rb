01a7b456d78f5d2b9bf0b8954d686f68074bdb30
Ion Gonzalez <ion@suzzlo.org>
2014-01-09 19:42:34 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportmania::Application.config.secret_key_base = 'e0fc791ecdde28e44562f4f873528298f40f3f03cb5f049c6bbcd2699b1238c35176427375fc3e3b49622a4a5c2cb0be514cea3ce6c5cbcff3d81885a05b21bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sportmania::Application.config.session_store :cookie_store, key: '_Sportmania_session'
